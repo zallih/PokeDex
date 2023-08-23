5d18e4d3758dca3c432ecadddd25cc4fcfaf6ebc
zallih <zappiellolailaz@gmail.com>
2023-08-23 17:24:02 -0300

    Subindo APi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {HttpClientModule} from '@angular/common/http'
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 //modules
@@ -21,7 +23,8 @@ import { PagesModule } from './pages/pages.module';
     BrowserModule,
     AppRoutingModule,
     PagesModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
 
+export class PokeListComponent implements OnInit{
+  
+  public getAllPokemons: any;
+
+
+  constructor(
+    private pokeApiService: PokeApiService
+  ){
+
+  }
+  ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => {
+        this.getAllPokemons = res.results;
+        console.log(this.getAllPokemons);
+      }
+    );
+  }
 }
